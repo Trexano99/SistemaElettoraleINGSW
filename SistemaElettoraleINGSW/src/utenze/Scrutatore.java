4d0066013b2e911ee6e1b15ae6389a49fb799af8
Shanti Ayala <shanti.ayala@studenti.unimi.it>
2021-11-03 10:40:16 +0100

    Aggiunta classe Scrutatore

---
+++ SistemaElettoraleINGSW/src/utenze/Scrutatore.java
@@ -0,0 +1,71 @@
+package utenze;
+
+import utenze.Utente.tipoUtente;
+
+/**
+ * Questa classe rappresenta uno Scrutatore del sistema di voto.
+ * 
+ * @author Shanti Ayala
+ *
+ */
+public final class Scrutatore {
+
+	Utente utenza;
+	
+	/**
+	 * Permette di creare un nuovo Scrutatore.
+	 * 
+	 * <p>
+	 * La sua creazione è subordinata dal passaggio come parametro
+	 * di un Utente che non sia nullo e che sia di tipologia
+	 * {@link utenze.Utente.tipoUtente}.
+	 * 
+	 * @param utenza
+	 */
+	public Scrutatore(Utente utenza) {
+		if (utenza == null)
+			throw new IllegalArgumentException("L'utente non può essere nullo");
+		if (!utenza.isTypeOfUser(tipoUtente.Scrutatore))
+			throw new IllegalArgumentException("L'utenza passata non è uno scrutatore");
+		this.utenza = utenza;
+	}
+	
+	/**
+	 * Lo scrutatore vuole istanziare una nuova sessione di Voto
+	 * 
+	 * @parama ....
+	 * @return ....
+	 */
+	
+	public void newVoteSession (/*@parama*/) {
+		
+		/*	Set dei parametri della sessione di voto:
+		 * 	-modalità voto
+		 * 	-calcolo vincitore 
+		 * 	-lista candidati (candidato inserito deve appartenere alla lista nel db, checkCandidato)
+		 *  
+		 * 	Raise msg se verificati problemi nel set dei parametri
+		 * */
+	}
+
+	
+	/**
+	 * Indica se l'utente/scrutatore può avviare la fase di scrutinio (Ballot)
+	 * 
+	 * @parama Scrutinio ballot per il quale si vuole sapere se la sessione è già attiva o no.
+	 * @return {@code true} se la sessione può essere avviata, ossia attualmente inattiva.
+	 */
+	public boolean canStartABallot (/*Scrutinio ballot*/) {
+		
+		/*
+		 * Query per richiedere lo scrutinio è gia avviato oppure no.
+		 * 
+		 * 
+		 * Return della risposta. 
+		 * */
+		return false;
+	}
+	
+	
+}
+
