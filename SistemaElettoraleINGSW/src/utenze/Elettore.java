bee20ff7b2b59b6e52db9e60ff6c134f705d370f
Shanti Ayala <shanti.ayala@studenti.unimi.it>
2021-11-10 23:30:58 +0100

    classe Elettore bozza1 per assignment3

--- SistemaElettoraleINGSW/src/utenze/Elettore.java
+++ SistemaElettoraleINGSW/src/utenze/Elettore.java
@@ -1,15 +1,168 @@
 package utenze;
 
 import utenze.Utente.tipoUtente;
+import java.lang.*;
 
 /**
  * Questa classe rappresenta un Elettore del sistema di voto.
  * 
- * @author Massimiliano Visconti
+ * @author Shanti Ayala
  *
  */
+/*Definire una classe Elettore che contenga i seguenti attributi e metodi: 
+• voto, un booleano che ha valore true nel caso in cui l’elettore abbia espresso il suo voto, false
+nel caso in cui l’elettore non abbia ancora espresso il voto
+• metodo esprimi_voto, di cui non è necessario dare l’implementazione completa (non mi 
+interessa sapere cosa abbia votato l’elettore, ma che abbia votato. Serve solo per poter specificare 
+alcune condizioni JML)*/
 public final class Elettore {
+	
+	//caratteristiche
+	private String nome, cognome, comune, nazione, sesso; 
+	private int[] dataNascita = new int[3]; //len 3
+	private char[] cf = new char[5]; 
+	private Boolean voto; 
+	
+	//getter&setter
+	public String getNome() {
+		return nome;
+	}
 
+
+	public void setNome(String nome) {
+		checkNome(nome); //nome non nullo
+		this.nome = nome;
+	}
+
+
+	public String getCognome() {
+		return cognome;
+	}
+
+
+	public void setCognome(String cognome) {
+		checkCognome(cognome); //cogn non nullo 
+		this.cognome = cognome;
+	}
+
+
+	public String getComune() {
+		return comune;
+	}
+
+
+	public void setComune(String comune) {
+		this.comune = comune;
+	}
+
+
+	public String getNazione() {
+		return nazione;
+	}
+
+
+	public void setNazione(String nazione) {
+		this.nazione = nazione;
+	}
+
+
+	public String getSesso() {
+		return sesso;
+	}
+
+
+	public void setSesso(String sesso) {
+		checkSesso(sesso); //sesso solo F o M
+		this.sesso = sesso;
+	}
+
+
+	public int[] getDataNascita() {
+		return dataNascita;
+	}
+
+
+	public void setDataNascita(int[] dataNascita) {
+		checkEta(dataNascita); //maggiorenne
+		this.dataNascita = dataNascita;
+	}
+
+
+	public char[] getCf() {
+		return cf;
+	}
+
+
+	public void setCf(char[] cf) {
+		this.cf = cf;
+	}
+
+
+	public Boolean getVoto() {
+		return voto;
+	}
+
+
+	public void setVoto(Boolean voto) {
+		this.voto = voto;
+	}
+	//JLM ------------------------------------------------------------------------------------------
+	
+		/**
+		 * 
+		 * @param 
+		 * @return
+		 */
+		private boolean checkNome(String nome){
+			if (nome == null)
+				throw new IllegalArgumentException("Il nome non può essere nullo");
+			return true;
+		}
+		
+		/**
+		 * 
+		 * @param 
+		 * @return
+		 */
+		private boolean checkCognome(String cognome){
+			if (cognome == null)
+				throw new IllegalArgumentException("Il cognome non può essere nullo");
+			return true;
+		}
+		
+		/**
+		 * 
+		 * @param 
+		 * @return
+		 */
+		private boolean checkSesso(String sesso) {
+			 if (sesso != "F" || sesso != "M")
+					throw new IllegalArgumentException("Il sesso può essere solo F o M");
+				return true; 
+		 
+		}
+		
+		
+		/**
+		 * @requires
+		 * @param 
+		 * @return
+		 */
+		private boolean checkEta(int[] dataNascita){
+			int eta =  calcolaEta(dataNascita);
+			if (eta < 18) //18 = maggiore eta in Italia!
+				throw new IllegalArgumentException("L'elettore non può essere minorenne");
+			return true;
+		}
+		
+		private int calcolaEta(int[] nascita) {
+			int eta = 0;
+			//usa qualche libreira per calcolare l'eta
+			
+			return eta;
+		}
+
+//--------------------------------------------------------------------------------------------------
 	//resta una cosa separata con il suo scopo unico quale comunicazione del DB.
 	//In altri casi ha poco senso avere come suoi attributi (vedi implementazione astratta) delle cose relative alla comunicazione con il DB.
 	Utente utenza;
@@ -32,7 +185,7 @@ public final class Elettore {
 		this.utenza = utenza;
 	}
 	
-	
+	//METODI ----------------------------------------------------------------------------------------
 	/**
 	 * Indica se l'utente può votare per una determinata elezione
 	 * 
@@ -64,4 +217,5 @@ public final class Elettore {
 		 * */
 		return false;
 	}
+	
 }
