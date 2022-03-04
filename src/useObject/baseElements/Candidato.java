a55fe73e7e0abc2d677d511c101823cb4759cd05
Massimiliano Visconti <viscontimax@hotmail.it>
2022-03-04 21:15:42 +0100

    Specializzati Candidato e Partito

--- src/useObject/baseElements/Candidato.java
+++ src/useObject/baseElements/Candidato.java
@@ -1,14 +1,30 @@
 package useObject.baseElements;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import daoModels.DbTablesRapresentation.Candidato_TB;
+import daoModels.ImplTablesDao.CandidatoDao;
+import useObject.voteElements.Elezione;
 
 public class Candidato {
 	
 	private int id;
+	
 	private String nome, cognome;
 	private Date dataNascita;
 	
 	
+	private Candidato(int id, String nome, String cognome, Date dataNascita) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataNascita = dataNascita;
+	}
+
+
 	public int getId() {
 		return id;
 	}
@@ -21,7 +37,26 @@ public class Candidato {
 	public Date getDataNascita() {
 		return dataNascita;
 	}
-
 	
+	public static List<Candidato> getAllCandidati(){
+		return listTabCandToCand(new CandidatoDao().getAllCandidati());
+	}
+	public static List<Candidato> getAllCandidatiPartito(Partito partito){	
+		return listTabCandToCand(new CandidatoDao().getCandidatiPartito(partito));
+	}
+	public static List<Candidato> getAllCandidatiElezione(Elezione elezione){
+		return listTabCandToCand(new CandidatoDao().getCandidatiElezione(elezione));
+	}
 	
+	private static List<Candidato> listTabCandToCand(List<Candidato_TB> candidati){
+		List<Candidato> listaCandidati = new ArrayList<Candidato>();
+		for (Candidato_TB candidato : candidati) {
+			listaCandidati.add(new Candidato(
+					candidato.getId(), 
+					candidato.getNome(), 
+					candidato.getCognome(), 
+					candidato.getDataNascita()));
+		}		
+		return listaCandidati;
+	}
 }
--- src/useObject/baseElements/Partito.java
+++ src/useObject/baseElements/Partito.java
@@ -1,18 +1,60 @@
 package useObject.baseElements;
 
-public class Partito {
+import java.util.ArrayList;
+import java.util.List;
 
-	int id;
-	String nome;
+import daoModels.DbTablesRapresentation.Candidato_TB;
+import daoModels.DbTablesRapresentation.Partito_TB;
+import daoModels.ImplTablesDao.CandidatoDao;
+import daoModels.ImplTablesDao.PartitoDao;
+import useObject.voteElements.Elezione;
+
+public class Partito {
 	
+	private int id;
+	private String nome;
+	private List<Candidato> candidatiPartito = new ArrayList<Candidato>();
 	
+	private Partito(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
 	public int getId() {
 		return id;
 	}
 	public String getNome() {
 		return nome;
 	}
+	public List<Candidato> getCandidatiPartito() {
+		return candidatiPartito;
+	}
 	
+	private void addPartitoCandidati(List<Candidato> candidati) {
+		this.candidatiPartito = candidati;
+	}
+	
+
+	public static List<Partito> getAllPartiti(){
+		return listTabPartToPart(new PartitoDao().getAllPartiti());
+	}
+	public static List<Partito> getAllPartitiElezione(Elezione elezione){
+		return listTabPartToPart(new PartitoDao().getAllPartitiElezione(elezione));
+	}
+	private static List<Partito> listTabPartToPart(List<Partito_TB> partiti){
+		List<Partito> listaPartiti = new ArrayList<Partito>();
+		for (Partito_TB partito : partiti) {
+			Partito p = new Partito(
+					partito.getId(),
+					partito.getNome()
+					);
+			p.addPartitoCandidati(Candidato.getAllCandidatiPartito(p));
+			listaPartiti.add(p);
+			
+		}		
+		return listaPartiti;
+	}
 	
 	
 }
