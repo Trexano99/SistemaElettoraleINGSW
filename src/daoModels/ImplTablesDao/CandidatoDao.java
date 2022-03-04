86904cbe9110e8ba0a48b7b383d2d338d9be3fcc
Massimiliano Visconti <viscontimax@hotmail.it>
2022-03-05 00:43:01 +0100

    Completate query richiesta informazioni

--- src/daoModels/ImplTablesDao/CandidatoDao.java
+++ src/daoModels/ImplTablesDao/CandidatoDao.java
@@ -1,30 +1,119 @@
 package daoModels.ImplTablesDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import auditing.LogElement;
+import auditing.LogHistory;
+import daoModels.DBConnector;
 import daoModels.DbTablesRapresentation.Candidato_TB;
+import daoModels.DbTablesRapresentation.Referendum_TB;
 import daoModels.InterfaceTablesDao.ICandidatoDao;
 import useObject.baseElements.Partito;
 import useObject.voteElements.Elezione;
+import useObject.voteElements.Votazione.TipologiaElezione;
 
 public class CandidatoDao implements ICandidatoDao {
 
 	@Override
 	public List<Candidato_TB> getAllCandidati() {
-		// TODO Auto-generated method stub
-		return FARE DOPO;
+		final String query = "SELECT * FROM sistemaelettoraleingsw.candidato;";
+		Connection dbConn = DBConnector.getDbConnection();
+		
+		List<Candidato_TB> tuttiCandidati = new ArrayList<Candidato_TB>();
+		
+		try {
+			
+			PreparedStatement preparedStmt = dbConn.prepareStatement(query);
+	
+			ResultSet reSet = preparedStmt.executeQuery();
+			
+			while(reSet.next()) {
+				tuttiCandidati.add(new Candidato_TB(
+						reSet.getInt(1),
+						reSet.getString(2),
+						reSet.getString(3),
+						reSet.getDate(4),
+						reSet.getInt(5)));
+			}
+			
+			return tuttiCandidati;
+			
+		} catch (SQLException e) {
+			LogHistory.getInstance().addLog(new LogElement(this, "SQLException", e.getSQLState(), true));
+		}
+		
+		return null;
 	}
 
 	@Override
 	public List<Candidato_TB> getCandidatiPartito(Partito partito) {
-		// TODO Auto-generated method stub
-		return FARE DOPO;
+		final String query = "SELECT * FROM sistemaelettoraleingsw.candidato WHERE partitoAppartenenza_fk = ?;";
+		Connection dbConn = DBConnector.getDbConnection();
+		
+		List<Candidato_TB> tuttiCandidati = new ArrayList<Candidato_TB>();
+		
+		try {
+			
+			PreparedStatement preparedStmt = dbConn.prepareStatement(query);
+			
+			preparedStmt.setInt(1, partito.getId());
+	
+			ResultSet reSet = preparedStmt.executeQuery();
+		
+			while(reSet.next()) {
+				tuttiCandidati.add(new Candidato_TB(
+						reSet.getInt(1),
+						reSet.getString(2),
+						reSet.getString(3),
+						reSet.getDate(4),
+						reSet.getInt(5)));
+			}
+			
+			return tuttiCandidati;
+			
+		} catch (SQLException e) {
+			LogHistory.getInstance().addLog(new LogElement(this, "SQLException", e.getSQLState(), true));
+		}
+		
+		return null;
 	}
 
 	@Override
-	public List<Candidato_TB> getCandidatiElezione(Elezione partito) {
-		// TODO Auto-generated method stub
-		return FARE DOPO;
+	public List<Candidato_TB> getCandidatiElezione(Elezione elezione) {
+		final String query = "SELECT c.* FROM sistemaelettoraleingsw.candidato c JOIN candidatielezione ce ON ce.candidato_fk = c.id WHERE ce.elezione_fk = ?;";
+		Connection dbConn = DBConnector.getDbConnection();
+		
+		List<Candidato_TB> tuttiCandidati = new ArrayList<Candidato_TB>();
+		
+		try {
+			
+			PreparedStatement preparedStmt = dbConn.prepareStatement(query);
+			
+			preparedStmt.setInt(1, elezione.getId());
+	
+			ResultSet reSet = preparedStmt.executeQuery();
+		
+			while(reSet.next()) {
+				tuttiCandidati.add(new Candidato_TB(
+						reSet.getInt(1),
+						reSet.getString(2),
+						reSet.getString(3),
+						reSet.getDate(4),
+						reSet.getInt(5)));
+			}
+			
+			return tuttiCandidati;
+			
+		} catch (SQLException e) {
+			LogHistory.getInstance().addLog(new LogElement(this, "SQLException", e.getSQLState(), true));
+		}
+		
+		return null;
 	}
 
 }
--- src/daoModels/ImplTablesDao/PartitoDao.java
+++ src/daoModels/ImplTablesDao/PartitoDao.java
@@ -1,7 +1,16 @@
 package daoModels.ImplTablesDao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import auditing.LogElement;
+import auditing.LogHistory;
+import daoModels.DBConnector;
+import daoModels.DbTablesRapresentation.Candidato_TB;
 import daoModels.DbTablesRapresentation.Partito_TB;
 import daoModels.InterfaceTablesDao.IPartitoDao;
 import useObject.voteElements.Elezione;
@@ -10,14 +19,58 @@ public class PartitoDao implements IPartitoDao {
 
 	@Override
 	public List<Partito_TB> getAllPartiti() {
-		// TODO Auto-generated method stub
-		return FARE DOPO;
+		final String query = "SELECT * FROM sistemaelettoraleingsw.partito;";
+		Connection dbConn = DBConnector.getDbConnection();
+		
+		List<Partito_TB> tuttiPartiti = new ArrayList<Partito_TB>();
+		
+		try {
+			
+			PreparedStatement preparedStmt = dbConn.prepareStatement(query);
+	
+			ResultSet reSet = preparedStmt.executeQuery();
+		
+			while(reSet.next()) {
+				tuttiPartiti.add(new Partito_TB(
+						reSet.getInt(1),
+						reSet.getString(2)));
+			}
+			
+			return tuttiPartiti;
+			
+		} catch (SQLException e) {
+			LogHistory.getInstance().addLog(new LogElement(this, "SQLException", e.getSQLState(), true));
+		}
+		
+		return null;
 	}
 
 	@Override
 	public List<Partito_TB> getAllPartitiElezione(Elezione elezione) {
-		// TODO Auto-generated method stub
-		return FARE DOPO;
+		final String query = "SELECT p.* FROM sistemaelettoraleingsw.partito p JOIN partitielezione pe ON p.id = pe.partito_fk WHERE p.id = ?;";
+		Connection dbConn = DBConnector.getDbConnection();
+		
+		List<Partito_TB> tuttiPartiti = new ArrayList<Partito_TB>();
+		
+		try {
+			
+			PreparedStatement preparedStmt = dbConn.prepareStatement(query);
+	
+			ResultSet reSet = preparedStmt.executeQuery();
+		
+			while(reSet.next()) {
+				tuttiPartiti.add(new Partito_TB(
+						reSet.getInt(1),
+						reSet.getString(2)));
+			}
+			
+			return tuttiPartiti;
+			
+		} catch (SQLException e) {
+			LogHistory.getInstance().addLog(new LogElement(this, "SQLException", e.getSQLState(), true));
+		}
+		
+		return null;
 	}
 
 }
