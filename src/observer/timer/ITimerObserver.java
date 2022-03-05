de13166a9d913796d16698e003eeb3a0a78f1be0
Massimiliano Visconti <viscontimax@hotmail.it>
2022-03-05 14:51:37 +0100

    Aggiunto Timer

---
+++ src/observer/timer/ITimerObserver.java
@@ -0,0 +1,9 @@
+package observer.timer;
+
+import java.util.Timer;
+
+public interface ITimerObserver {
+
+	public void thickedTimer(Timer timer);
+	
+}
---
+++ src/observer/timer/TimerTicker.java
@@ -0,0 +1,60 @@
+package observer.timer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerTicker  {
+	
+	private Timer timer;
+	private int seconds;
+	private List<ITimerObserver> observers = new ArrayList<ITimerObserver>();
+
+	private TimerTicker(int durata,  List<ITimerObserver>observers) {
+		this.timer = new Timer();
+		this.seconds = durata;
+		for (ITimerObserver obs : observers) {
+			addObserver(obs);
+		}
+		timer.schedule(timerTask, 1, durata);
+	}
+	
+	
+	private void addObserver(ITimerObserver obs) {
+		this.observers.add(obs);
+	}
+	
+	private void removeObserver(ITimerObserver obs) {
+		this.observers.remove(obs);
+	}
+	
+	private void timerTicked() {
+		for (ITimerObserver obs : observers) {
+			obs.thickedTimer(timer);
+		}
+	}
+	
+	private TimerTask timerTask = new TimerTask() {
+	    @Override
+	    public void run() {
+	        timerTicked();
+	    }
+	};
+	
+	public int getSeconds() {
+		return seconds;
+	}
+	public String getSecondsString() {
+		return String.format("%02d:%02d", (seconds % 3600)/60, seconds%60);
+	}
+	
+	private static TimerTicker instance;
+
+	public static void createTimer(int durata, List<ITimerObserver>observers) {
+		instance = new TimerTicker(durata,observers);
+	}
+	
+	
+	
+}
