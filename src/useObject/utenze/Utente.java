f7ceb6eeb2e4dedca9b0dae59ec682d12c4185cc
Shanti Ayala <72122315+NanoConfusoo@users.noreply.github.com>
2022-03-09 23:32:26 +0100

    Update Utente.java

--- src/useObject/utenze/Utente.java
+++ src/useObject/utenze/Utente.java
@@ -12,7 +12,19 @@ public abstract class Utente {
 	protected String cognome;
 	
 	public abstract List<Votazione> getAllVotazioni();
-
+	/*{context Utente
+	 * inv: self.allIstances() -> isUnique(id)
+	 * inv lunghezzaNome: nome.size() > 1
+	 * inv lunghezzaCognome: cognome.size() > 1
+	 * }*/
+	
+	/*@ 
+	 @ invariant nome.length() >1;
+	 @ invariant cognome.length() >1;
+	 @ assignable id;
+	 @ assignable nome;
+	 @ assignable cognome;
+	 @*/
 	protected Utente(String id, String nome, String cognome) {
 		if(id==null||nome==null||cognome==null) 
 			throw new IllegalArgumentException("Fields cannot be null");
@@ -21,15 +33,20 @@ public abstract class Utente {
 		this.nome = nome;
 		this.cognome = cognome;
 	}
-
+	/*@ ensures \result == id;
+	 @*/
 	public String getId() {
 		return id;
 	}
-
+	
+	/*@ ensures \result == nome;
+	 @*/
 	public String getNome() {
 		return nome;
 	}
-
+	
+	/*@ ensures \result == cognome;
+	 @*/
 	public String getCognome() {
 		return cognome;
 	}
